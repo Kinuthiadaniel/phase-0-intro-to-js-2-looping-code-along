5c12350c0eb3c8af4d568b53e60a029ba041efb6
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-25 15:26:09 +0300

    intial

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+// for(let age=30; age <40; age++){
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+// const gifts = ["teady bear","drone","doll"]
+// function wrapGifts(gifts){
+// for(let i = 0; i<gifts.length; i++){
+//   console.log (`Wrapped ${gifts[i]} and added a bow!`);
+ 
+// }
+// return gifts;
+// }
+// wrapGifts(gifts);
+
+const gifts = ["teddy bear","doll","drone"]
+function wrapGifts(gifts){
+  let i = 0;
+  while (i < gifts.length){
+    console.log(`Wrapped ${gifts[i]} and added a bow`); i++;
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+
+let i;
+function countDown(i){
+  while( i >= 0){
+    console.log (i); i--;
+}
+return i;
+}
+(countDown(10));
